ad46e06986f420f25fea854764d8b56ee871f003
shaberle <shaberlem@gmail.com>
2023-09-28 13:50:07 -0300

    primera idea tarea 3

--- Tarea3/T3/prev.c
+++ Tarea3/T3/prev.c
@@ -3,5 +3,21 @@
 #include "prev.h"
 
 void asignarPrev(Nodo *t, Nodo **pprev) {
-  ...
+  if(*t->izq == NULL && *t->prev == NULL){
+    *t->prev = *pprev;
+    *t->prox = NULL;
+    *pprev = *t;
+    asignarPrev(*t-)
+  }
+  else if(*t->izq == NULL && *t->prev != NULL){
+    *t->prox = NULL;
+    *t->prev = *pprev;
+    *t->prox = NULL;
+    *pprev = *t;
+    asignarPrev(*t-)
+  }
+  else{
+  asignarPrev(*t->izq, &*pprev);
+  }
+
 }
