c16a0a9c46e59363f82422d66d65414c639f0f45
shaberle <shaberlem@gmail.com>
2023-09-28 18:12:19 -0300

    Segundo intento Tarea 3

--- Tarea3/T3/prev.c
+++ Tarea3/T3/prev.c
@@ -1,23 +1,37 @@
 #include <stddef.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "prev.h"
 
+
 void asignarPrev(Nodo *t, Nodo **pprev) {
-  if(*t->izq == NULL && *t->prev == NULL){
-    *t->prev = *pprev;
-    *t->prox = NULL;
-    *pprev = *t;
-    asignarPrev(*t-)
+  if(t == NULL){
+    return;
   }
-  else if(*t->izq == NULL && *t->prev != NULL){
-    *t->prox = NULL;
-    *t->prev = *pprev;
-    *t->prox = NULL;
-    *pprev = *t;
-    asignarPrev(*t-)
+  else if(t->izq == NULL && *pprev == NULL){
+    t->prev = *pprev;
+    t->prox = NULL;
+    *pprev = t;
+    printf("primer nodo=%d\n",t->x);
+    asignarPrev(t->der, &*pprev);
+  }
+  else if(t->izq == NULL && *pprev != NULL){
+    t->prev = *pprev;
+    (t->prev)->prox = t;
+    t->prox = NULL;
+    *pprev = t;
+    printf("segundo nodo=%d\n",t->x);
+    printf("segundo prev=%d\n",(t->prev)->x);
+    asignarPrev(t->der,&*pprev);
   }
   else{
-  asignarPrev(*t->izq, &*pprev);
+  asignarPrev(t->izq, &*pprev);
+  (t->izq)->prox = t;
+  *pprev = t->izq;
+  t->prev = *pprev;
+  t->prox = NULL;
+  *pprev = t;
+  printf("Ultimo nodo= %d\n",(t->prev)->x);
+  asignarPrev(t->der,&*pprev);
   }
-
 }
